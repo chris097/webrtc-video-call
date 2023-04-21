1630f7858a32f5a8ff94e451b12aaa29d7cc48f5
chris097 <chrisfidel.international@gmail.com>
2023-04-21 10:05:30 +0100

    peer to peer connection

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+const cors = require("cors");
+
+const port = process.env.PORT || 8000;
+
+// app
+
+const rooms = {};
+
+app.use(cors())
+
+io.on("connection", socket => {
+    socket.on("join room", roomID => {
+        if (rooms[roomID]) {
+            rooms[roomID].push(socket.id)
+        } else {
+            rooms[roomID] = [socket.id];
+        }
+        const otherUser = rooms[roomID].find(id => id !== socket.id);
+        if (otherUser) {
+            socket.emit("other user", otherUser);
+            socket.to(otherUser).emit("user joined", socket.id)
+        }
+    })
+
+    // sending offer
+    socket.on("offer", payload => {
+        io.to(payload.target).emit("offer", payload)
+    })
+
+    // answer
+    socket.on("answer", payload => {
+        io.to(payload.target).emit("answer", payload)
+    })
+
+    socket.on("ice-candidate", incoming => {
+        io.to(incoming.target).emit("ice-candidate", incoming.candidate)
+    })
+})
+
+server.listen(port, () => console.log(`listening on port ${port}`))
